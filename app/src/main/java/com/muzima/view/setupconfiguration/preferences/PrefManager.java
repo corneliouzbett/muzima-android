2e110fe628a6bb5a0ef1093b1792066262b297e2
corneliouzBett <kbett68@gmail.com>
2018-06-20 10:53:40 +0300

    added user customization preferences manager

---
+++ app/src/main/java/com/muzima/view/setupconfiguration/preferences/PrefManager.java
@@ -0,0 +1,37 @@
+package com.muzima.view.setupconfiguration.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by corneliouzBett on 15/07/18.
+ */
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "muzima-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
