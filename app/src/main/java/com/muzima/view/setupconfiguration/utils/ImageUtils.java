ce0dd913338646a156baa46a9682cff06b127051
corneliouzBett <kbett68@gmail.com>
2018-06-20 13:55:17 +0300

    added image utils Encoding and decoding Bitmaps

---
+++ app/src/main/java/com/muzima/view/setupconfiguration/utils/ImageUtils.java
@@ -0,0 +1,29 @@
+package com.muzima.view.setupconfiguration.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImageUtils {
+
+    // encoding
+    public static String encodeTobase64(Bitmap image) {
+        Bitmap immage = image;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        immage.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] b = baos.toByteArray();
+        String imageEncoded = Base64.encodeToString(b, Base64.DEFAULT);
+
+        Log.d("Image Log:", imageEncoded);
+        return imageEncoded;
+    }
+    // decoding
+    public static Bitmap decodeBase64(String input) {
+        byte[] decodedByte = Base64.decode(input, 0);
+        return BitmapFactory
+                .decodeByteArray(decodedByte, 0, decodedByte.length);
+    }
+}
